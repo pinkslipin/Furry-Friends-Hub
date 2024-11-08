49de8bca11c3fd8f91649308f850dea163121afb
MrCareerBully <mrcareerbully@gmail.com>
2024-11-08 11:07:03 +0800

    pasting medicalrecord entity,repo,service, and controller

---
+++ masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Repository/MedicalRecordRepository.java
@@ -0,0 +1,23 @@
+package com.furryfriends.masterbackend.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.furryfriends.masterbackend.Entity.MedicalRecordEntity;
+import com.furryfriends.masterbackend.Entity.VetEntity;
+
+@Repository
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecordEntity, Integer>{
+
+//this is user-defined method to search a student record by lastname
+
+//public MedicalRecordEntity findByLname(String lname);
+
+//you may define more methods for searching, for instance, in this interface
+    //public List<MedicalRecordEntity> findByP
+    List<MedicalRecordEntity> findAllByVet(VetEntity vet);
+    //List<MedicalRecordEntity> findAllByPet(PetEntity pet);
+}
---
+++ masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Service/MedicalRecordService.java
@@ -0,0 +1,122 @@
+package com.furryfriends.masterbackend.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.furryfriends.masterbackend.Entity.MedicalRecordEntity;
+import com.furryfriends.masterbackend.Repository.MedicalRecordRepository;
+
+/*
+Hannah uncomment ni if naa ang imo repo
+import com.furryfriends.masterbackend.Entity.PetEntity;
+import com.furryfriends.masterbackend.Repository.PetRepository;
+*/
+
+import com.furryfriends.masterbackend.Entity.VetEntity;
+import com.furryfriends.masterbackend.Repository.VetRepository;
+
+@Service
+
+public class MedicalRecordService {
+
+    @Autowired
+    MedicalRecordRepository mrrepo;
+
+	/*
+	@Autowired
+	PetRepository prepo;
+	*/
+
+	@Autowired
+	VetRepository vrepo;
+
+    public MedicalRecordService() {
+        super();
+    //TODO Auto-generated constructor stub
+    }
+
+    //Create of CRUD
+    public MedicalRecordEntity postMedicalRecord(int petid, int vetid, MedicalRecordEntity medicalRecord) {
+		/*
+		PetEntity pet = prepo.findById(petid).orElseThrow(() -> new NoSuchElementException("Pet with ID " + petid + " not found"));
+		*/
+
+		VetEntity vet = vrepo.findById(vetid).orElseThrow(() -> new NoSuchElementException("Vet with ID " + vetid + " not found"));
+
+		//medicalRecord.setPet(pet);
+		medicalRecord.setVet(vet);
+
+
+        return mrrepo.save(medicalRecord);
+    }
+
+    //Read of CRUD
+
+    public List<MedicalRecordEntity> getAllMedicalRecords(){
+        return mrrepo.findAll();
+    }
+	
+	public MedicalRecordEntity getMedicalRecord(int id){
+		return mrrepo.findById(id).orElseThrow(() -> 
+            new NoSuchElementException("Medical Record with ID " + id + " not found")
+        );
+	}
+
+	public List<MedicalRecordEntity> getPetMedicalRecords(int petid){
+		//PetEntity pet = prepo.findById(petid).orElseThrow(() -> new NoSuchElementException("Pet with ID " + petid + " not found"));
+		return mrrepo.findAll();
+	}
+
+	public List<MedicalRecordEntity> getVetMedicalRecords(int vetid){
+		VetEntity vet = vrepo.findById(vetid).orElseThrow(() -> new NoSuchElementException("Vet with ID " + vetid + " not found"));
+
+		return mrrepo.findAllByVet(vet);
+	}
+    
+    //Update of CRUD	
+	public MedicalRecordEntity putMedicalRecordDetails(int petid, int vetid, int id, MedicalRecordEntity newMedicalRecordDetails) {
+		MedicalRecordEntity medicalRecord = new MedicalRecordEntity();
+		
+		//search id
+		medicalRecord = mrrepo.findById(id).orElseThrow(() -> 
+        	new NoSuchElementException("Medical Record with ID " + id + " not found")
+    	);
+
+		//PetEntity pet = prepo.findById(petid).orElseThrow(() -> new NoSuchElementException("Pet with ID " + petid + " not found"));
+		VetEntity vet = vrepo.findById(vetid).orElseThrow(() -> new NoSuchElementException("Vet with ID " + vetid + " not found"));
+
+		/*
+		if(medicalRecord.getPet().getPid() != pet.getPid()){
+			medicalRecord.setPet(pet);
+		}
+		*/
+
+		if(medicalRecord.getVet().getVetid() != vet.getVetid()){
+			medicalRecord.setVet(vet);
+		}
+			
+		//for setting new values (only when the id exists)
+		medicalRecord.setRecordDate(newMedicalRecordDetails.getRecordDate());
+		medicalRecord.setMedicalProcedure(newMedicalRecordDetails.getMedicalProcedure());
+		medicalRecord.setMedication(newMedicalRecordDetails.getMedication());
+		medicalRecord.setNotes(newMedicalRecordDetails.getNotes());
+
+		return mrrepo.save(medicalRecord);
+	}
+	
+	//Delete of CRUD
+	public String deleteMedicalRecord(int id) {
+		String msg = "";
+		if(mrrepo.existsById(id)) {
+			mrrepo.deleteById(id);
+			msg = "Medical Record successfully deleted";
+		}
+		else msg = "Medical Record with ID " + id + " not found";
+		
+		return msg;
+	}
+}
