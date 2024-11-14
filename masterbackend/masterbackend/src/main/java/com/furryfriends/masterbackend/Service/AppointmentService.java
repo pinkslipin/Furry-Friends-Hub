0b7c01f1ba42ea92e5cf6b80e0c050c367606ba8
AdrianJhudiel <jhudzartezuela@gmail.com>
2024-11-14 20:21:46 +0800

    Update AppointmentService.java
    added errorhandlings appointmentservice

--- masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Service/AppointmentService.java
+++ masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Service/AppointmentService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.furryfriends.masterbackend.Entity.AppointmentEntity;
 import com.furryfriends.masterbackend.Entity.BillingEntity;
@@ -30,27 +32,37 @@ public class AppointmentService {
     @Autowired
     private BillingRepository billingRepository;
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+        public class ResourceNotFoundException extends RuntimeException {
+            private static final long serialVersionUID = 1L;
+
+            public ResourceNotFoundException(String message) {
+        super(message);
+        }
+    }
     // Create
     public AppointmentEntity createAppointment(AppointmentEntity appointment) {
         if (appointment.getVets() != null && appointment.getVets().getVetid() != 0) {
-            VetEntity vet = vetRepository.findById(appointment.getVets().getVetid()).orElse(null);
-            if (vet != null) {
-                appointment.setVets(vet);
-            }
+            VetEntity vet = vetRepository.findById(appointment.getVets().getVetid())
+                .orElseThrow(() -> new ResourceNotFoundException("Vet not found with id: " + appointment.getVets().getVetid()));
+            appointment.setVets(vet);
         }
+        
 
         if (appointment.getPet() != null && appointment.getPet().getPid() != 0) {
-            PetEntity pet = petRepository.findById(appointment.getPet().getPid()).orElse(null);
-            if (pet != null) {
-                appointment.setPet(pet);
-            }
+            PetEntity pet = petRepository.findById(appointment.getPet().getPid())
+                    .orElseThrow(() -> new ResourceNotFoundException("Pet not found with id " + appointment.getPet().getPid()));
+            appointment.setPet(pet);
         }
-        if (appointment.getBilling() != null && appointment.getBilling().getBillingId() != 0) {
-            BillingEntity billing = billingRepository.findById(appointment.getBilling().getBillingId()).orElse(null);
-            if (billing != null) {
+
+        if (appointment.getBilling() != null) {
+            BillingEntity billing = new BillingEntity();
+            billing.setAmountDue(appointment.getBilling().getAmountDue());
+            billing.setAmountPaid(appointment.getBilling().getAmountPaid());
+            billing = billingRepository.save(billing);
                 appointment.setBilling(billing);
             }
-        }
+
         return appointmentRepository.save(appointment);
     }
 
