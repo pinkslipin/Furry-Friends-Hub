24f49b4f0c6342ec0ca325f1c7cbfd2123187a06
pinkslipin <craigmattc@gmail.com>
2024-11-15 11:40:30 +0800

    small changes

--- masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Entity/PetEntity.java
+++ masterbackend/masterbackend/src/main/java/com/furryfriends/masterbackend/Entity/PetEntity.java
@@ -4,6 +4,16 @@ import jakarta.persistence.*;
 import java.util.List;
 //import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
