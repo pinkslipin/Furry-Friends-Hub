d1d350beb76d5d095c768a0aa5bc43b9a250d513
pinkslipin <craigmattc@gmail.com>
2024-11-19 02:38:31 +0800

    Added styling to owner and vet portal

--- masterfrontend/src/components/VetLoginorSignupPage.jsx
+++ masterfrontend/src/components/VetLoginorSignupPage.jsx
@@ -1,13 +1,36 @@
 // VetLoginorSignupPage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Box } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
+import logo from '../images/logo.png';
+import Paw from '../images/Paw.png';
+import './OwnerHome.css';
 
 const VetLoginorSignupPage = () => (
     <Box sx={{ textAlign: 'center', padding: '20px' }}>
-        <Button component={Link} to="/VetLogin" variant="outlined" color="primary">
-            Vet Login
-        </Button>
+        <div className="homepage">
+            <header className="content">
+                <div className="welcome-message">
+                    <h1>
+                        <img src={logo} alt="FurryFriends Hub Logo" className="logo-image2" />
+                        Welcome to <span>FurryFriends Hub</span>
+                        <br></br>
+                        Vet Portal
+                    </h1>
+                    <div className="paw-prints">
+                        <span>
+                            <img src={Paw} alt="Paw Print" className="paw-image" />
+                        </span>
+                        <span>
+                            <img src={Paw} alt="Paw Print" className="logo-image2" />
+                        </span>
+                    </div>
+                </div>
+            </header>
+            <Button component={Link} to="/VetLogin" variant="outlined" color="primary">
+                Vet Login
+            </Button>
+        </div>
     </Box>
 );
 
